bc441f354ebdff45c11035eacb606f2398cb3e60
carlos jose caceres ochoa <caceresochoacarlosjose@gmail.com>
2023-07-27 09:51:46 -0500

    agregando nombre de jar

--- src/main/java/com/desaextremo/retotres/controller/GadgetController.java
+++ src/main/java/com/desaextremo/retotres/controller/GadgetController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/gadget")
+@RequestMapping("/api/gadget")
 @CrossOrigin(origins = "*")
 public class GadgetController {
     @Autowired
--- src/main/java/com/desaextremo/retotres/controller/UserController.java
+++ src/main/java/com/desaextremo/retotres/controller/UserController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/user")
+@RequestMapping("/api/user/")
 @CrossOrigin(origins = "*")
 public class UserController {
     @Autowired
